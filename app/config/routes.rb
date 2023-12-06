39d50af217587d946fce65987a06546bd19c8eb3
Bruno Montebello <brunormontebello@hotmail.com>
2023-12-06 14:06:12 +0000

    Add carts + order_item routes

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/manage_items', as: 'manage_items'
   resources :items
+  resources :order_items
+  resources :carts, only: %i[show]
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 
