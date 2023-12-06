399dfde34b29be366c9a0a062612c408cc4860d5
Bruno Montebello <brunormontebello@hotmail.com>
2023-12-06 14:10:06 +0000

    DRY order_items controller

--- app/app/controllers/order_items_controller.rb
+++ app/app/controllers/order_items_controller.rb
@@ -1,13 +1,30 @@
 class OrderItemsController < ApplicationController
+  before_action :set_order, only: %i[create update destroy]
+
   def create
-    @order = current_order
     @order_item = @order.order_items.new(order_item_params)
     @order.save
 
     session[:order_id] = @order.id
   end
 
+  def update
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.update(order_item_params)
+    @order_items = @order.order_items
+  end
+
+  def destroy
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.destroy
+    @order_items = @order.order_items
+  end
+
   private
+
+  def set_order
+    @order = current_order
+  end
 
   def order_item_params
     params.require(:order_item).permit(:item_id, :quantity)
