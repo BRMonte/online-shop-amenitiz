aaf30d616c9615a4e79d9d518ae453fe7dacda45
Bruno Montebello <brunormontebello@hotmail.com>
2023-12-07 16:33:09 +0000

    Add order model spec

--- app/spec/models/order_spec.rb
+++ app/spec/models/order_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Order.new }
+
+  describe 'associations' do
+    it 'should have many order_items' do
+      t = Order.reflect_on_association(:order_items)
+      expect(t.macro).to eq(:has_many)
+    end
+  end
+
+  describe 'callbacks' do
+    it 'sets subtotal before saving' do
+      order_item = create(:order_item, order: subject)
+      expect(subject.subtotal).to eq(order_item.total_price)
+    end
+  end
 end
