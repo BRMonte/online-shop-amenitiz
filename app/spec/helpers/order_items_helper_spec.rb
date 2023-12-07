546efc04618574ca9960c7506d72564af708b7d0
Bruno Montebello <brunormontebello@hotmail.com>
2023-12-07 17:15:58 +0000

    Add order_item helpers spec

--- app/spec/helpers/order_items_helper_spec.rb
+++ app/spec/helpers/order_items_helper_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the OrderItemsHelper. For example:
-#
-# describe OrderItemsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe OrderItemsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:order_item) { create(:order_item) }
+  let(:order_items) { create_list(:order_item, 3) }
+
+  describe '#order_has_items?' do
+    it 'returns true if order has items' do
+      expect(helper.order_has_items?(order_items)).to eq(true)
+    end
+
+    it 'returns false if order has no items' do
+      empty_order_items = []
+      expect(helper.order_has_items?(empty_order_items)).to eq(false)
+    end
+  end
+
+  describe '#get_item_name' do
+    it 'returns the name for the order item' do
+      name = order_item.item.name
+      expect(helper.get_item_name(order_item)).to eq(name)
+    end
+  end
 end
