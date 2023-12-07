7c28b52948ff83628a53abd128355754b1c36877
Bruno Montebello <brunormontebello@hotmail.com>
2023-12-07 15:32:58 +0000

    Complete Item spec

--- app/spec/factories/items.rb
+++ app/spec/factories/items.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    price { 1.5 }
-    code { 'UP2' }
-    slug { name.parameterize }
+    sequence(:name) { |n| "Item #{n}" }
+    price { rand(0.1..15.0).round(2) }
+
+    trait :with_code do
+      code { CodeGeneratorService.new(self, { letters: 2, numbers: 1 }).call }
+    end
+
+    factory :item_with_code do
+      with_code
+    end
   end
 end
--- app/spec/models/item_spec.rb
+++ app/spec/models/item_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  include FactoryBot::Syntax::Methods
+
   let(:item) { FactoryBot.build(:item) }
 
-  context 'validations' do
+  it 'has a valid factory' do
+    expect(create(:item)).to be_valid
+  end
+
+  describe 'associations' do
+    it 'has many order items' do
+      association = described_class.reflect_on_association(:order_items)
+      expect(association.macro).to eq :has_many
+    end
+  end
+
+  describe 'validations' do
     it 'is valid with valid attributes' do
       expect(item).to be_valid
     end
@@ -17,15 +30,32 @@ RSpec.describe Item, type: :model do
       item.price = nil
       expect(item).not_to be_valid
     end
+  end
 
-    it 'is not valid without a code' do
-      item.code = nil
-      expect(item).not_to be_valid
+  describe 'scopes' do
+    let!(:item1) { create(:item, name: 'B', price: 10) }
+    let!(:item2) { create(:item, name: 'A', price: 5) }
+
+    it 'returns ordered by name' do
+      expect(described_class.alphabetical).to eq([item2, item1])
     end
 
-    it 'is not valid without a slug' do
-      item.slug = nil
-      expect(item).not_to be_valid
+    it 'returns ordered by price' do
+      expect(described_class.by_price).to eq([item2, item1])
+    end
+  end
+
+  describe 'methods' do
+    describe '.search' do
+      let!(:item1) { create(:item, name: 'Green Shirt') }
+
+      it 'returns all items when empty' do
+        expect(described_class.search(nil)).to eq([item1])
+      end
+
+      it 'returns matching item by name' do
+        expect(described_class.search('green')).to eq([item1])
+      end
     end
   end
 end
